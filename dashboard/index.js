5d7b16006ef50c5ee9ae1c0463145c0ea7635003
batman1292 <batman12922@gmail.com>
2016-06-09 14:34:29 +0700

    start using jade

--- dashboard/index.js
+++ dashboard/index.js
@@ -1,33 +1,22 @@
-//lets require/import the mongodb native drivers.
-var mongodb = require('mongodb');
+var express = require('express');
+var path = require('path');
+var mongoose = require('mongoose');
+var app = express();
 
-//We need to work with "MongoClient" interface in order to connect to a mongodb server.
-var MongoClient = mongodb.MongoClient;
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-// Connection URL. This is where your mongodb server is running.
-var url = 'mongodb://localhost:27017/mqtt';
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
-// Use connect method to connect to the Server
-MongoClient.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    //HURRAY!! We are connected. :)
-    console.log('Connection established to', url);
-    // console.log(db);
-    // do some work here with the database.
-    var collection = db.collection('devices');
-    collection.find().toArray(function (err, result) {
-      if (err) {
-        console.log(err);
-      } else if (result.length) {
-        console.log('Found:', result);
-      } else {
-        console.log('No document(s) found with defined "find" criteria!');
-      }
-      db.close();
-    });
-    //Close connection
-    //
-  }
+
+function getHomePage(req, res) {
+    res.render('index.jade');
+}
+
+app.get('/', getHomePage);
+
+var server = app.listen(3000, function() {
+    console.log('Express.js is running...');
 });
